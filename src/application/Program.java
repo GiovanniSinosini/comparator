7ce05eeb657e168bd060e7d95901790669ea9452
Giovanni Sinosini <carvalho.sino@gmail.com>
2020-01-27 07:32:28 +0000

    Demo3 optimized

--- src/application/Program.java
+++ src/application/Program.java
@@ -16,10 +16,7 @@ public class Program {
 		list.add(new Product("Tablet", 400.0));
 		
 		
-		Comparator<Product> comp = (p1, p2) -> {
-			return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-		
-		};
+		Comparator<Product> comp = (p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
 		
 		list.sort(comp);
 		
