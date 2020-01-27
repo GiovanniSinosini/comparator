2a2dbf037507df1f2f1bd8a7e2fcd71be689b03c
Giovanni Sinosini <carvalho.sino@gmail.com>
2020-01-27 07:24:55 +0000

    Demo2 -> ordination within the main

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import entities.Product;
@@ -14,7 +15,17 @@ public class Program {
 		list.add(new Product("Notebook", 1200.0));
 		list.add(new Product("Tablet", 400.0));
 		
-		list.sort(new MyComparator());;
+		
+		Comparator<Product> comp = new Comparator<Product>() {
+
+			@Override
+			public int compare(Product p1, Product p2) {
+				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+			}
+			
+		};
+		
+		list.sort(comp);
 		
 		for (Product p : list) {
 			System.out.println(p);
